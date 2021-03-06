e54a1e362f1730b3e68653279b5f32ba06e29e57
Anisha McDowell <anishamcdowell@gmail.com>
2021-03-06 14:10:50 -0500

    Load screen complete

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,11 +1,24 @@
 var getStartedEl = $("#get-started-btn");
 var introEl = $("#intro");
-var gifEl = $("#gif-board");
+var gifBoardEl = $("#gif-board");
+var loadingEl = $("#loading");
+var gifsEl = $(".gif");
+
 
 console.log("Js is working");
 
 //Get started button > load gif board
 $("#get-started-btn").click(function() {
-    console.log("Get started was clicked");
     window.location.href = "gifboard.html";
   });
+
+//Hook for any gif being selected
+gifsEl.click(function() {
+  //On click > gifboard and h3 are hidden
+  gifBoardEl.addClass("is-hidden");
+  $("#gifboard-h3").addClass("is-hidden");
+  //Loading H3 and load bar are displayed
+  loadingEl.removeClass("is-hidden");
+  $("#loading-h3").removeClass("is-hidden");
+  
+});
