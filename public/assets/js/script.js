2b11778c7c57b05469c4d6a9777f86d9dd51983b
Sureshot0341 <gregpetropoulos@yahoo.com>
2021-03-08 22:09:23 -0500

    Worked on the api call to the giphy with fetch and its working

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,51 +1,43 @@
 var getStartedEl = $("#get-started-btn");
 var introEl = $("#intro");
 var gifEl = $("#gif-board");
+var giphyGridItems = []
+var BASE_URL = `https://api.giphy.com/v1/gifs/search?api_key=DmJrVZsNraIXZqq3u6JXaUIkz1kcRShZ&limit=1&offset=${offset}&rating=g&lang=en`;
+var searchTerm = ['happy', 'sad', 'funny', 'mad', 'confused', 'inspirational', 'excited', 'lazy', 'jealous']
+var offset = Math.floor(Math.random() * 4999);
+
 
 console.log("Js is working");
 
-//Get started button > load gif board
-// $("#get-started-btn").click(function () {
-//   console.log("Get started was clicked");
-//   window.location.href = "gifboard.html";
+// Get started button > load gif board
+$("#get-started-btn").click(function () {
+  console.log("Get started was clicked");
+  window.location.href = "gifboard.html";
 
-// });
+});
 
-// Setting up fetch for giphy grid
+// Setting up fetch for giphy grid call and spotify call
 
 
-function giphyGrid(requestUrl) {
-
-  var requestUrl = 'https://api.giphy.com/v1/gifs/trending?api_key=1IZwxvtq8OEMv6ULkhlXoRgjfNmlONFv&limit=9&rating=r'
-
-  fetch(requestUrl)
-    .then(function (response) {
-      return response.json();
+function giphyGrid(searchTerm) {
+  // loop over the searchTerm array
+  for (var i = 0; i < 10; i++) {
+    fetchBasedOnEmotion(searchTerm[i]);
+  }
+  
+}
+// 
+function fetchBasedOnEmotion(emo) {
+  fetch(`${BASE_URL}&q=${emo}`)
+    .then(function(res) {
+      return res.json()
+    }).then(function(data) {      
+      var innerHTML = $(`<img src=${data.data[0].images.original.url}/>`)
+      var searchId = `#${emo}`;
+      $(`#${emo}`).append(innerHTML);
     })
-    .then(function (data) {
-      console.log(data)
-
-    ,then(function imageGrab() {
-        
-        var gifs = $('.gif');
-        $(gifs).each(function () {
-          var dataUrl = data[i].url
-          $('img').attr({ 'src': dataUrl }
-        }
-      }
-
-for (var i = 0; i < data.length; i++) {
-       
-       
-        $('img').attr({ 'src': dataUrl })
-
-      });
 
 }
 
 
-    })
-
-
-}
-giphyGrid();
+giphyGrid(searchTerm);
